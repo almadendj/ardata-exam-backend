e3bd8b143a3b06bc88d7124dcf3742620cb8224a
almadendj <almadendj@gmail.com>
2024-09-19 01:00:41 +0800

    improve error handling

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,15 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use url::ParseError;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
     NoApiKey,
+    ProviderError(String),
+    UrlParseError(String),
 }
 
 impl IntoResponse for Error {
@@ -15,5 +18,19 @@ impl IntoResponse for Error {
         println!("->> {:<12} - {self:?}", "INTO_RES_ERROR");
 
         (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+    }
+}
+
+impl From<ParseError> for Error {
+    fn from(value: ParseError) -> Self {
+        Error::UrlParseError(value.to_string())
+    }
+}
+
+impl From<ethers::providers::ProviderError> for Error {
+    fn from(value: ethers::providers::ProviderError) -> Self {
+        println!("->> {:<12} - {value:?}", "ETHERS_ERROR");
+
+        Error::ProviderError(value.to_string())
     }
 }
