c7b12a753d33a85234573321d16e5aa5f83ab5ab
almadendj <almadendj@gmail.com>
2024-09-19 00:45:14 +0800

    move api_key to dotenv

--- src/web/handlers.rs
+++ src/web/handlers.rs
@@ -1,6 +1,9 @@
+use std::env;
+
 use crate::{Error, Result};
 use axum::response::{Html, IntoResponse};
 use axum::Json;
+use dotenv::dotenv;
 use ethers::providers::{Middleware, Provider};
 use serde_json::{json, Value};
 
@@ -11,21 +14,18 @@ pub async fn hello_world() -> impl IntoResponse {
 }
 
 pub async fn get_gas_price() -> Result<Json<Value>> {
+    dotenv().ok();
     println!("->> {:<12} - get_gas_price", "HANDLER");
 
-    // TODO: move to env
-    let api_key = "wzaVIkJ_jhBVL-CxeUDOZdHvfzz9lliP";
-
-    if api_key.is_empty() {
-        return Err(Error::NoApiKey);
-    }
+    let api_key = match env::var("API_KEY") {
+        Ok(api_key) => api_key,
+        Err(_) => return Err(Error::NoApiKey),
+    };
 
     let rpc_url = format!("https://eth-mainnet.g.alchemy.com/v2/{api_key}");
     let provider = Provider::try_from(rpc_url).unwrap();
 
     let gas_price = provider.get_gas_price().await.unwrap();
-
-    println!("gas price: {gas_price}");
 
     let body = Json(json!({
         "result": {
