b26413d8ffe60f39bd5ef9c9a8e72ca32e30e279
almadendj <almadendj@gmail.com>
2024-09-18 10:08:45 +0800

    add serde, serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::net::SocketAddr;
 
-use axum::response::Html;
+use axum::extract::Query;
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
@@ -17,4 +16,17 @@ async fn main() {
     println!("->> LISTENING on {addr}\n");
 
     axum::serve(listener, routes_hello).await.unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+
+    Html(format!("Hello <string>{name}</strong>"))
 }
