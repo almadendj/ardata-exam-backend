efb14c582a55d5044b7da01a48ba5171356814e2
almadendj <almadendj@gmail.com>
2024-09-18 10:20:54 +0800

    add additional sample routes, add static routing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::net::SocketAddr;
 
-use axum::extract::Query;
+use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse};
-use axum::routing::get;
+use axum::routing::{get, get_service};
 use axum::Router;
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_hello = Router::new()
+        .merge(routes())
+        .fallback_service(routes_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
@@ -16,6 +19,16 @@ async fn main() {
     println!("->> LISTENING on {addr}\n");
 
     axum::serve(listener, routes_hello).await.unwrap();
+}
+
+fn routes() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello_path))
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
 
 #[derive(Debug, Deserialize)]
@@ -28,5 +41,11 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
 
     let name = params.name.as_deref().unwrap_or("World!");
 
-    Html(format!("Hello <string>{name}</strong>"))
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello_path(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello_path - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
 }
