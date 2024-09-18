d3c1289bea5be468c6e534148d76a1a013fbc3ed
almadendj <almadendj@gmail.com>
2024-09-18 10:37:52 +0800

    add get gas price route

--- src/web/routes.rs
+++ src/web/routes.rs
@@ -5,11 +5,17 @@ use axum::{
 };
 
 pub fn routes() -> Router {
-    Router::new().route("/", get(hello_world))
+    Router::new()
+        .route("/", get(hello_world))
+        .route("/get-gas-price", get(get_gas_price))
 }
 
 async fn hello_world() -> impl IntoResponse {
     println!("->> {:<12} - hello_world_new", "HANDLER");
 
     Html("Hello World!!!!!")
+}
+
+async fn get_gas_price() {
+    println!("->> {:<12} - get_gas_price", "HANDLER");
 }
