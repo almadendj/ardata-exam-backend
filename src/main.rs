329d69a7582cd6ad3c457d9d1e39ebe9ef0d694f
almadendj <almadendj@gmail.com>
2024-09-23 17:07:03 +0800

    add CORS, make any for now

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::net::SocketAddr;
 
 pub use self::error::{Error, Result};
+use axum::http::Method;
 use axum::routing::get_service;
 use axum::Router;
+use tower_http::cors::CorsLayer;
 use tower_http::services::ServeDir;
 
 mod error;
@@ -10,9 +12,15 @@ mod web;
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new()
+        .allow_origin(tower_http::cors::Any) // allow any origin for now
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers(tower_http::cors::Any);
+
     let routes = Router::new()
         .merge(web::routes::routes())
-        .fallback_service(routes_static());
+        .fallback_service(routes_static())
+        .layer(cors);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
